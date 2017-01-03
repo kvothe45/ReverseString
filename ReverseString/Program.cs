b70c996bcae637faee170242f0d52336343d4165
Ralph E. Beard IV <ralph.e.beard.iv@outlook.com>
2017-01-03 15:13:21 -0500

    Add files via upload

---
+++ ReverseString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Give me a string");
+            string originalString = Console.ReadLine();
+            char[] newString = new char[originalString.Length];
+            int count = 0;
+
+            
+
+            for (int i = originalString.Length -1; i >= 0; i--)
+            {
+                newString[count] = originalString[i];
+                count++;
+                
+            }
+
+            foreach (char element in newString)
+                Console.Write(element);
+            Console.WriteLine();
+            Console.WriteLine(originalString);
+            //Console.ReadLine();
+        }
+    }
+}
